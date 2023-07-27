040d396df2cf3d2535b143e64e5ad4beede8a145
Maksymilian Robak <maksiur1234@gmail.com>
2023-07-27 13:58:09 +0200

    add react

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-import './bootstrap';
+// resources/js/app.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
